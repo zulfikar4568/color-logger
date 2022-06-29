fd2223f2dd63a6994bc7378ef9719c5024014e05
zulfikar4568 <isnaen70@gmail.com>
2022-06-29 14:02:11 +0700

    1.0.1

--- src/__tests__/logger.test.ts
+++ src/__tests__/logger.test.ts
@@ -1,9 +1,9 @@
-import { awesomeLogger } from '../index'
+import { awesomeLogger } from '../index';
 
-const logger = awesomeLogger('My Application :','silly', true, 'logs/','my-app-log', 'error', true, true, true, true)
+const logger = awesomeLogger('My Application :', 'silly', true, 'logs/', 'my-app-log', 'error', true, true, true, true);
 
 test('Logger Error', () => {
-  logger.error = jest.fn()
-  logger.error('Hello Error')
+  logger.error = jest.fn();
+  logger.error('Hello Error');
   expect(logger.error).toHaveBeenCalledWith('Hello Error');
-})
+});
